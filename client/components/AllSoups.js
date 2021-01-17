e79f23c7564f9b2a8fcd0d46b1d5720204d426c7
Ian Walls <wallsianh@gmail.com>
2021-01-17 14:16:46 -0500

    [added] remove soup functionality to all soups view

--- client/components/AllSoups.js
+++ client/components/AllSoups.js
@@ -2,10 +2,27 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {fetchAllSoups} from '../store/allSoups'
 import {Link} from 'react-router-dom'
+import axios from 'axios'
 
 export class AllSoups extends Component {
+  constructor() {
+    super()
+    this.removeSoup = this.removeSoup.bind(this)
+  }
+
   componentDidMount() {
     this.props.fetchAllSoupsInReact()
+  }
+
+  async removeSoup(evt) {
+    evt.preventDefault()
+    try {
+      const soupId = evt.target.name
+      await axios.delete(`/api/soups/${soupId}`)
+      this.props.fetchAllSoupsInReact()
+    } catch (err) {
+      console.log(err)
+    }
   }
 
   render() {
@@ -16,29 +33,39 @@ export class AllSoups extends Component {
 
     const soupsInReact = this.props.soups.soups
 
-    return (
-      <div>
-        {this.props.isAdmin ? <h1>Inventory</h1> : <h1>All Soups</h1>}
-        {soupsInReact.map(soup => {
-          const {id, name, quantity, imageUrl} = soup
-          return (
-            <div key={id}>
-              <Link to={`soups/${soup.id}`}>
-                <h2>
-                  {name}: {quantity} cans in stock
-                </h2>
-                <img src={imageUrl} style={imageStyle} />
-              </Link>
-              {this.props.isAdmin ? (
-                <button type="submit">Remove Soup From Stock</button>
-              ) : (
-                <button type="submit">Add to Cart</button>
-              )}
-            </div>
-          )
-        })}
-      </div>
-    )
+    if (soupsInReact.length > 0) {
+      return (
+        <div>
+          {this.props.isAdmin ? <h1>Inventory</h1> : <h1>All Soups</h1>}
+          {soupsInReact.map(soup => {
+            const {id, name, quantity, imageUrl} = soup
+            return (
+              <div key={id}>
+                <Link to={`soups/${soup.id}`}>
+                  <h2>
+                    {name}: {quantity} cans in stock
+                  </h2>
+                  <img src={imageUrl} style={imageStyle} />
+                </Link>
+                {this.props.isAdmin ? (
+                  <button
+                    type="submit"
+                    name={soup.id}
+                    onClick={this.removeSoup}
+                  >
+                    Remove Soup From Stock
+                  </button>
+                ) : (
+                  <button type="submit">Add to Cart</button>
+                )}
+              </div>
+            )
+          })}
+        </div>
+      )
+    } else {
+      return <h1>There Are No Soups In Stock</h1>
+    }
   }
 }
 
