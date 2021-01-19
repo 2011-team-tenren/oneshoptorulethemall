5c08807d07b0b2a4485bd01196ba956144baabd5
angiepmunoz <angiepmunoz@gmail.com>
2021-01-19 10:48:56 -0500

    removed console logs

--- client/components/GuestCart.js
+++ client/components/GuestCart.js
@@ -8,65 +8,58 @@ export class GuestCart extends Component {
     super()
     this.state = {
       qty: 0,
-      cart: {}
+      cart: order
     }
     this.editQtyClick = this.editQtyClick.bind(this)
     this.editQtyChange = this.editQtyChange.bind(this)
     this.removeSoupClick = this.removeSoupClick.bind(this)
   }
 
-  componentDidMount() {}
-
-  editQtyClick(soup, evt) {
+  async editQtyClick(soup, evt) {
     evt.preventDefault()
-    // let retrievedOrder = JSON.parse(localStorage.getItem('order'))
-    // let order = Object.values(retrievedOrder)
-    // console.log('order', order)
     let updatedOrder = {}
-    order.map(currentSoup => {
+    let orderArr = order.map(currentSoup => {
       if (currentSoup.id === soup.id) {
         currentSoup.orderQuantity = this.state.qty
       }
       updatedOrder[currentSoup.flavor] = currentSoup
       return currentSoup
     })
-    // let stringOrder =
-    console.log('UPDATED ORDER:', updatedOrder)
-    this.setState({cart: updatedOrder})
-    console.log('CART STATE', this.state)
+    await this.setState({cart: orderArr})
     localStorage.setItem('order', JSON.stringify({...updatedOrder}))
   }
 
-  removeSoupClick(soup, evt) {
+  async removeSoupClick(soup, evt) {
     evt.preventDefault()
     let updatedOrder = {}
-    order.filter(currentSoup => {
+    let orderArr = order.filter(currentSoup => {
       if (currentSoup.id !== soup.id) {
         updatedOrder[currentSoup.flavor] = currentSoup
         return currentSoup
       }
     })
     console.log(updatedOrder)
+    await this.setState({cart: orderArr})
     localStorage.setItem('order', JSON.stringify({...updatedOrder}))
     console.log(localStorage)
   }
 
   editQtyChange(event) {
     this.setState({[event.target.name]: Number(event.target.value)})
-    // console.log('qty state', this.state)
   }
 
   render() {
-    // let retrievedOrder = JSON.parse(localStorage.getItem('order'))
-    // let order = Object.values(retrievedOrder)
-    console.log('order in render', order)
+    const imageStyle = {
+      height: '20rem',
+      width: 'auto'
+    }
     return (
       <div>
-        {order.map((soup, idx) => {
+        {this.state.cart.map((soup, idx) => {
           return (
             <div key={idx}>
               <h1>{soup.name}</h1>
-              <img src={soup.imageUrl} />
+              <img src={soup.imageUrl} style={imageStyle} />
               <h2>Price: ${soup.price / 100}</h2>
               <h2>Quantity: {soup.orderQuantity}</h2>
               <form>
@@ -107,7 +100,7 @@ export class GuestCart extends Component {
           )
         })}
         <div>
-          <button>Checkout</button>
+          <button type="submit">Checkout</button>
         </div>
       </div>
     )
