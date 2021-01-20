5aa61886c0a969656f500387cb87f45c11d8f6b2
dahyun <61565989+cleverplumtea@users.noreply.github.com>
2021-01-19 22:03:17 -0500

    Revert "Am/guest cart wip"

--- client/components/GuestCart.js
+++ client/components/GuestCart.js
@@ -1,141 +1,14 @@
 import React, {Component} from 'react'
-import axios from 'axios'
 
 export class GuestCart extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      qty: 0,
-      cart: Object.values(JSON.parse(localStorage.getItem('order')))
-    }
-    this.editQtyClick = this.editQtyClick.bind(this)
-    this.editQtyChange = this.editQtyChange.bind(this)
-    this.removeSoupClick = this.removeSoupClick.bind(this)
-    this.checkoutClick = this.checkoutClick.bind(this)
-  }
-
-  async editQtyClick(soup, evt) {
-    evt.preventDefault()
-    let updatedOrder = {}
-    let orderArr = this.state.cart.map(currentSoup => {
-      if (currentSoup.id === soup.id) {
-        currentSoup.orderQuantity = this.state.qty
-      }
-      updatedOrder[currentSoup.flavor] = currentSoup
-      return currentSoup
-    })
-    await this.setState({cart: orderArr})
-    localStorage.setItem('order', JSON.stringify({...updatedOrder}))
-  }
-
-  editQtyChange(event) {
-    this.setState({[event.target.name]: Number(event.target.value)})
-  }
-
-  async removeSoupClick(soup, evt) {
-    evt.preventDefault()
-    let updatedOrder = {}
-    let orderArr = this.state.cart.filter(currentSoup => {
-      if (currentSoup.id !== soup.id) {
-        updatedOrder[currentSoup.flavor] = currentSoup
-        return currentSoup
-      }
-    })
-    await this.setState({cart: orderArr})
-    localStorage.setItem('order', JSON.stringify({...updatedOrder}))
-  }
-
-  async checkoutClick(event) {
-    event.preventDefault()
-    alert(' Your order has been placed.  Please check your email.')
-    try {
-      const order = await axios.post('/api/orders/guest/checkout')
-      let idx = 0
-      while (idx < this.state.cart.length) {
-        await axios.put(
-          `/api/orders/${order.data.id}/soups/${this.state.cart[idx].id}`,
-          {
-            quantity: this.state.cart[idx].orderQuantity
-          }
-        )
-        idx++
-      }
-      let emptyCart = JSON.stringify({})
-      localStorage.setItem('order', emptyCart)
-      this.setState({cart: []})
-    } catch (error) {
-      console.error(error)
-    }
+  componentDidMount() {
+    console.log(localStorage)
   }
 
   render() {
-    const imageStyle = {
-      height: '20rem',
-      width: 'auto'
-    }
-    let cart = this.state.cart || []
-
-    return (
-      <div>
-        {cart.length ? (
-          <div>
-            {cart.map((soup, idx) => {
-              return (
-                <div key={idx}>
-                  <h1>{soup.name}</h1>
-                  <img src={soup.imageUrl} style={imageStyle} />
-                  <h2>Price: ${soup.price / 100}</h2>
-                  <h2>Quantity: {soup.orderQuantity}</h2>
-                  <form>
-                    <label htmlFor="Edit Quantity">Edit Quantity</label>
-                    <input
-                      name="qty"
-                      type="number"
-                      max="30"
-                      min="0"
-                      value={this.state.qty}
-                      onChange={this.editQtyChange}
-                    />
-                    <button
-                      type="submit"
-                      onClick={() => {
-                        this.editQtyClick(soup, event)
-                      }}
-                    >
-                      Update
-                    </button>
-                    <div>
-                      Remove {soup.flavor} soups:{' '}
-                      <button
-                        type="submit"
-                        onClick={() => this.removeSoupClick(soup, event)}
-                      >
-                        {' '}
-                        X{' '}
-                      </button>
-                    </div>
-                  </form>
-                  <h2>
-                    {soup.flavor} Total Price: $
-                    {soup.price * soup.orderQuantity / 100}
-                  </h2>
-                  <h2 />
-                </div>
-              )
-            })}
-            <div>
-              <button
-                type="submit"
-                onClick={event => this.checkoutClick(event, cart)}
-              >
-                Checkout
-              </button>
-            </div>
-          </div>
-        ) : (
-          <h1>There are currently no soups in your cart!</h1>
-        )}
-      </div>
-    )
+    let retrievedOrder = localStorage.getItem('order')
+    let order = Object.values(JSON.parse(retrievedOrder))
+    console.log(order)
+    return <div>Hello</div>
   }
 }
