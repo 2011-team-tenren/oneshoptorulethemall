fd22d26b2d4e20a5c5fd77f936521101d4c2c0be
dahyun lee <lee.dahyun@gmail.com>
2021-01-21 10:49:52 -0500

    additional styling

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -15,7 +15,11 @@ export const UserHome = props => {
       <Link to={`/orderhistory/${id}`}>
         <h4>View your order history</h4>
       </Link>
-      <img src="https://i.pinimg.com/originals/4f/a9/cc/4fa9ccde0d43629a97b1029c4f65fbd7.png" />
+      <img
+        width="300"
+        height="300"
+        src="https://lh3.googleusercontent.com/XBdzhXfWR6UTe6Dj0SeyJH3RgUeKFpSm4u5be14RLk5C7KmCt-_ytmg0vGsRfowfH2jfxmArzrnVxsT-Gw5WksKRDv46PqxjdGax7TsC-pH8mzkvVanAcLBnfVEw_iMurRAMPPHoSQ=w2400"
+      />
       <p>All your favorite soups in one place</p>
     </div>
   )
